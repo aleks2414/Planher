9571eb7d0fb91dd60746d99f1d78ea92f0eb547b
aleks2414 <alejandroromo14@gmail.com>
2016-06-06 21:40:31 -0500

    Pruebas2

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,6 +3,8 @@ class Contact < MailForm::Base
 	attribute :name, 					:validate => true
 	attribute :email, 				:validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
 	attribute :message, 			:validate => true
+	attribute :gas,						:validate => true
+	attribute :mant,					:validate => true
 	attribute :nickname, 			:captcha => true
 
 def headers 
